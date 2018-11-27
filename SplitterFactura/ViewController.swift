553ecf36bb568a00c83777180c3190aadf7f0683
Raul Villarreal <raulvilla0797@gmail.com>
2018-11-26 19:52:37 -0700

    Initial Commit

---
+++ SplitterFactura/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SplitterFactura
+//
+//  Created by Raul Ernesto Villarreal Sigala on 11/26/18.
+//  Copyright © 2018 Raul Ernesto Villarreal Sigala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
